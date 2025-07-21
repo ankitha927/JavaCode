094e43b79fca02db1964ede2a181afcfdd7d4959
ankitha927 <rankitha927@gmail.com>
2025-07-21 11:49:25 +0530

    Added Remove duplicate

---
+++ RemoveDup.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class RemoveDup {
+    public int removeDup(int[] num){
+        int slow=num[0];
+        int fast=num[0];
+        do { 
+            slow=num[slow];
+            fast=num[num[fast]];
+        } while (slow!=fast);
+
+        slow=num[0];
+        while(slow!=fast){
+            slow=num[slow];
+            fast=num[fast];
+        }
+        return slow;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size: ");
+        int n=sc.nextInt();
+        System.out.println("ENter the elements: ");
+        int[] num=new int[n];
+        for(int i=0;i<n;i++){
+            num[i]=sc.nextInt();
+
+        }
+        RemoveDup obj=new RemoveDup();
+        int ans=obj.removeDup(num);
+        System.out.println("The duplicate no is: "+ans);
+        
+    }
+    
+}
